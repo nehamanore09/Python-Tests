8cdbdaf77a71be9d87b5e3006c07ba5e9848882e
nehamanore09 <136306140+nehamanore09@users.noreply.github.com>
2026-01-30 11:26:00 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,14 @@
+a = float(input ("Enter the first number: "))
+b = float(input ("Enter the second number: "))
+
+Addition = a + b
+Subtraction = a - b
+Multiplication = a * b
+Division = a / b
+Module = a % b
+
+print("Addition = " , Addition)
+print("Subtraction = " , Subtraction)
+print("Multiplication = " , Multiplication)
+print("Division = " , Division)
+print("Module = " , Module)
